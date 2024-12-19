5d3f1d2292bb4da3deb0e8e21416fa301b075769
Maaz Khan <maxiikhan04@gmail.com>
2024-12-19 18:03:46 +0500

    minor change

--- src/layout/underconstruction.js
+++ src/layout/underconstruction.js
@@ -4,7 +4,7 @@ import { HeaderMainlogo } from '../constants/header/menuData.ts';
 
 const UnderConstruction = () => {
   return (
-    <div className="flex flex-col items-center h-[100vh] justify-center bg-gray-100 text-gray-800">
+    <div className="flex flex-col items-center h-[100vh] justify-center bg-gray-100 text-gray-800 px-3">
         <img
                           src={HeaderMainlogo.headerMainLogo}
                           alt="Header Logo"
@@ -13,8 +13,8 @@ const UnderConstruction = () => {
       <div className="text-6xl text-[#e70000] animate-bounce">
         <FaTools />
       </div>
-      <h1 className="mt-4 text-4xl font-bold">We're Under Construction</h1>
-      <p className="mt-2 text-lg text-gray-600">
+      <h1 className="mt-4 text-4xl font-bold text-center">We're Under Construction</h1>
+      <p className="mt-2 text-lg text-gray-600 text-center">
         This page is currently being built. Check back soon for updates!
       </p>
     </div>
